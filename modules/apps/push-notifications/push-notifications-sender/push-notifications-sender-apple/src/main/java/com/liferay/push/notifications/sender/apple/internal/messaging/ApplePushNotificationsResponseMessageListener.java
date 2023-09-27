d87d9d0adc257cccd2073de6390a9d6a997763cc
Renan Vasconelos <renan.vasconcelos@liferay.com>
2023-09-27 14:19:06 -0300

    LPS-197625 Merge ApplePushNotificationsMessagingConfigurator with ApplePushNotificationsResponseMessageListener and apply to usages

--- modules/apps/push-notifications/push-notifications-sender/push-notifications-sender-apple/src/main/java/com/liferay/push/notifications/sender/apple/internal/messaging/ApplePushNotificationsResponseMessageListener.java
+++ modules/apps/push-notifications/push-notifications-sender/push-notifications-sender-apple/src/main/java/com/liferay/push/notifications/sender/apple/internal/messaging/ApplePushNotificationsResponseMessageListener.java
@@ -7,9 +7,17 @@ package com.liferay.push.notifications.sender.apple.internal.messaging;
 
 import com.liferay.portal.kernel.messaging.BaseMessageListener;
 import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageListener;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.push.notifications.constants.PushNotificationsDestinationNames;
 import com.liferay.push.notifications.exception.PushNotificationsException;
 import com.liferay.push.notifications.sender.Response;
 import com.liferay.push.notifications.sender.apple.internal.AppleResponse;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
 
 /**
  * @author Bruno Farache
@@ -26,6 +34,23 @@ public class ApplePushNotificationsResponseMessageListener
 		}
 
 		throw new PushNotificationsException(response.getStatus());
+	}
+
+	private static final ServiceRegistration<MessageListener>
+		_serviceRegistration;
+
+	static {
+		Bundle bundle = FrameworkUtil.getBundle(
+			ApplePushNotificationsResponseMessageListener.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		_serviceRegistration = bundleContext.registerService(
+			MessageListener.class,
+			new ApplePushNotificationsResponseMessageListener(),
+			MapUtil.singletonDictionary(
+				"destination.name",
+				PushNotificationsDestinationNames.PUSH_NOTIFICATION_RESPONSE));
 	}
 
 }
