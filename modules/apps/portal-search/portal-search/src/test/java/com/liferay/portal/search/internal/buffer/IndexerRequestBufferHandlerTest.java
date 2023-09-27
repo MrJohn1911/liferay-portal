54e688208618b46ea9cb15d118ad1ce875f6d3fd
Renan Vasconelos <renan.vasconcelos@liferay.com>
2023-09-27 15:09:04 -0300

    LPS-197625 Change IndexerRequestBufferExecutor to a Util class and apply to usages

--- modules/apps/portal-search/portal-search/src/test/java/com/liferay/portal/search/internal/buffer/IndexerRequestBufferHandlerTest.java
+++ modules/apps/portal-search/portal-search/src/test/java/com/liferay/portal/search/internal/buffer/IndexerRequestBufferHandlerTest.java
@@ -5,8 +5,8 @@
 
 package com.liferay.portal.search.internal.buffer;
 
+import com.liferay.portal.kernel.module.util.SystemBundleUtil;
 import com.liferay.portal.kernel.search.Indexer;
-import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.search.configuration.IndexerRegistryConfiguration;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
@@ -16,10 +16,16 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
 
 /**
  * @author Bryan Engler
@@ -31,6 +37,22 @@ public class IndexerRequestBufferHandlerTest {
 	public static LiferayUnitTestRule liferayUnitTestRule =
 		LiferayUnitTestRule.INSTANCE;
 
+	@BeforeClass
+	public static void setUpClass() {
+		BundleContext bundleContext = SystemBundleUtil.getBundleContext();
+
+		Mockito.when(
+			FrameworkUtil.getBundle(Mockito.any())
+		).thenReturn(
+			bundleContext.getBundle()
+		);
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+		_frameworkUtilMockedStatic.close();
+	}
+
 	public IndexerRequestBufferHandlerTest() throws Exception {
 		_method = Indexer.class.getDeclaredMethod(
 			"reindex", String.class, long.class);
@@ -41,7 +63,7 @@ public class IndexerRequestBufferHandlerTest {
 		int maxBufferSize = 5;
 
 		_indexerRequestBufferHandler = new IndexerRequestBufferHandler(
-			_createIndexerRequestBufferOverflowHandler(),
+			new IndexerRequestBufferOverflowHandler(),
 			_createIndexerRegistryConfiguration(maxBufferSize));
 
 		_indexerRequestBuffer = IndexerRequestBuffer.create();
@@ -76,21 +98,6 @@ public class IndexerRequestBufferHandlerTest {
 		return new IndexerRequest(
 			_method, indexer, RandomTestUtil.randomString(),
 			RandomTestUtil.randomLong());
-	}
-
-	private IndexerRequestBufferOverflowHandler
-		_createIndexerRequestBufferOverflowHandler() {
-
-		IndexerRequestBufferOverflowHandler
-			indexerRequestBufferOverflowHandler =
-				new IndexerRequestBufferOverflowHandler();
-
-		ReflectionTestUtil.setFieldValue(
-			indexerRequestBufferOverflowHandler,
-			"_indexerRequestBufferExecutor",
-			new IndexerRequestBufferExecutor());
-
-		return indexerRequestBufferOverflowHandler;
 	}
 
 	private List<IndexerRequest> _createIndexerRequests(
@@ -129,6 +136,9 @@ public class IndexerRequestBufferHandlerTest {
 		_indexerRequestBufferHandler.bufferRequest(
 			indexerRequest, _indexerRequestBuffer);
 	}
+
+	private static final MockedStatic<FrameworkUtil>
+		_frameworkUtilMockedStatic = Mockito.mockStatic(FrameworkUtil.class);
 
 	private final Indexer<?> _indexer = Mockito.mock(Indexer.class);
 	private IndexerRequestBuffer _indexerRequestBuffer;
