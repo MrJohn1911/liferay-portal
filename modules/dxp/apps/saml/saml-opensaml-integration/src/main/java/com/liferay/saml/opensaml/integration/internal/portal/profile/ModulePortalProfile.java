a45724f00a2baccdbcb852d699494a02ca24b820
Joao Victor <joao.victor@liferay.com>
2024-01-24 09:13:28 -0300

    LPS-199593 make MembershipsUserFieldExpressionHandler be published as a UserFieldExpressionHandler, use enabled = false property, delete MembershipsUserFieldExpressionHandlerRegistrator and create ModulePortalProfile to enable the component when the FeatureFlag is not enabled

---
+++ modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/portal/profile/ModulePortalProfile.java
@@ -0,0 +1,52 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.saml.opensaml.integration.internal.portal.profile;
+
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManager;
+import com.liferay.portal.profile.BaseDSModulePortalProfile;
+import com.liferay.portal.profile.PortalProfile;
+import com.liferay.saml.opensaml.integration.internal.field.expression.handler.MembershipsUserFieldExpressionHandler;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Joao Victor Alves
+ */
+@Component(service = PortalProfile.class)
+public class ModulePortalProfile extends BaseDSModulePortalProfile {
+
+	@Activate
+	protected void activate(ComponentContext componentContext) {
+		Set<String> supportedPortalProfileNames = null;
+
+		if (!_featureFlagManager.isEnabled("LPS-180198")) {
+			supportedPortalProfileNames = Collections.emptySet();
+		}
+		else {
+			supportedPortalProfileNames = new HashSet<>();
+
+			supportedPortalProfileNames.add(
+				PortalProfile.PORTAL_PROFILE_NAME_CE);
+			supportedPortalProfileNames.add(
+				PortalProfile.PORTAL_PROFILE_NAME_DXP);
+		}
+
+		init(
+			componentContext, supportedPortalProfileNames,
+			MembershipsUserFieldExpressionHandler.class.getName());
+	}
+
+	@Reference
+	private FeatureFlagManager _featureFlagManager;
+
+}
