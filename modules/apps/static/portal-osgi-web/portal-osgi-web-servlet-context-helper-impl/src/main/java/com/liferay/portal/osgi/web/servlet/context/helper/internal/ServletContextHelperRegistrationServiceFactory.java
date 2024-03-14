f7daecbfe0e8702e4a0a636a03fb0be49f7816b0
Joao Victor <joao.victor@liferay.com>
2024-03-14 18:14:04 -0300

    AAAA

--- modules/apps/static/portal-osgi-web/portal-osgi-web-servlet-context-helper-impl/src/main/java/com/liferay/portal/osgi/web/servlet/context/helper/internal/ServletContextHelperRegistrationServiceFactory.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-servlet-context-helper-impl/src/main/java/com/liferay/portal/osgi/web/servlet/context/helper/internal/ServletContextHelperRegistrationServiceFactory.java
@@ -5,33 +5,31 @@
 
 package com.liferay.portal.osgi.web.servlet.context.helper.internal;
 
+import com.liferay.petra.executor.PortalExecutorManager;
+import com.liferay.petra.reflect.ReflectionUtil;
 import com.liferay.portal.osgi.web.servlet.JSPServletFactory;
 import com.liferay.portal.osgi.web.servlet.context.helper.ServletContextHelperRegistration;
 
-import java.util.Map;
-import java.util.concurrent.ExecutorService;
+import java.util.HashMap;
 
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParserFactory;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.ServiceFactory;
 import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import org.xml.sax.SAXNotRecognizedException;
+import org.xml.sax.SAXNotSupportedException;
 
 /**
  * @author Raymond Augé
  */
+@Component(service = ServiceFactory.class)
 public class ServletContextHelperRegistrationServiceFactory
 	implements ServiceFactory<ServletContextHelperRegistration> {
-
-	public ServletContextHelperRegistrationServiceFactory(
-		JSPServletFactory jspServletFactory, SAXParserFactory saxParserFactory,
-		Map<String, Object> properties, ExecutorService executorService) {
-
-		_jspServletFactory = jspServletFactory;
-		_saxParserFactory = saxParserFactory;
-		_properties = properties;
-		_executorService = executorService;
-	}
 
 	@Override
 	public ServletContextHelperRegistration getService(
@@ -39,9 +37,30 @@ public class ServletContextHelperRegistrationServiceFactory
 		ServiceRegistration<ServletContextHelperRegistration>
 			serviceRegistration) {
 
+		_saxParserFactory.setNamespaceAware(false);
+		_saxParserFactory.setValidating(false);
+		_saxParserFactory.setXIncludeAware(false);
+
+		try {
+			_saxParserFactory.setFeature(_FEATURES_DISALLOW_DOCTYPE_DECL, true);
+			_saxParserFactory.setFeature(
+				_FEATURES_EXTERNAL_GENERAL_ENTITIES, false);
+			_saxParserFactory.setFeature(
+				_FEATURES_EXTERNAL_PARAMETER_ENTITIES, false);
+			_saxParserFactory.setFeature(_FEATURES_LOAD_EXTERNAL_DTD, false);
+		}
+		catch (ParserConfigurationException | SAXNotRecognizedException |
+			   SAXNotSupportedException exception) {
+
+			ReflectionUtil.throwException(exception);
+		}
+
 		return new ServletContextHelperRegistrationImpl(
-			bundle, _jspServletFactory, _saxParserFactory, _properties,
-			_executorService);
+			bundle, _jspServletFactory, _saxParserFactory,
+			new HashMap<String, Object>(),
+			_portalExecutorManager.getPortalExecutor(
+				ServletContextHelperRegistrationServiceFactory.class.
+					getName()));
 	}
 
 	@Override
@@ -54,9 +73,25 @@ public class ServletContextHelperRegistrationServiceFactory
 		servletContextHelperRegistration.close();
 	}
 
-	private final ExecutorService _executorService;
-	private final JSPServletFactory _jspServletFactory;
-	private final Map<String, Object> _properties;
-	private final SAXParserFactory _saxParserFactory;
+	private static final String _FEATURES_DISALLOW_DOCTYPE_DECL =
+		"http://apache.org/xml/features/disallow-doctype-decl";
+
+	private static final String _FEATURES_EXTERNAL_GENERAL_ENTITIES =
+		"http://xml.org/sax/features/external-general-entities";
+
+	private static final String _FEATURES_EXTERNAL_PARAMETER_ENTITIES =
+		"http://xml.org/sax/features/external-parameter-entities";
+
+	private static final String _FEATURES_LOAD_EXTERNAL_DTD =
+		"http://apache.org/xml/features/nonvalidating/load-external-dtd";
+
+	@Reference
+	private JSPServletFactory _jspServletFactory;
+
+	@Reference
+	private PortalExecutorManager _portalExecutorManager;
+
+	@Reference
+	private SAXParserFactory _saxParserFactory;
 
 }
