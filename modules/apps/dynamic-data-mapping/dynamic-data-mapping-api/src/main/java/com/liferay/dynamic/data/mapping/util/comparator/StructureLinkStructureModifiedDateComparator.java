165787e88505979cb448b722ebdd6f9c21902180
Joao Victor <joao.victor@liferay.com>
2024-05-21 11:07:10 -0300

    LPD-26679 Transform StructureLinkStructureModifiedDateComparator into a singleton, make the constructor private and apply to usage
    # breaking
    
    ## What modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/util/comparator/StructureLinkStructureModifiedDateComparator.java
    
    the class is now a singleton
    
    ## Why
    
    this change was made to improve the performance of the querys that uses this comparator
    
    ----

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/util/comparator/StructureLinkStructureModifiedDateComparator.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/util/comparator/StructureLinkStructureModifiedDateComparator.java
@@ -25,8 +25,14 @@ public class StructureLinkStructureModifiedDateComparator
 
 	public static final String[] ORDER_BY_FIELDS = {"modifiedDate"};
 
-	public StructureLinkStructureModifiedDateComparator(boolean ascending) {
-		_ascending = ascending;
+	public static StructureLinkStructureModifiedDateComparator getInstance(
+		boolean ascending) {
+
+		if (ascending) {
+			return _INSTANCE_ASCENDING;
+		}
+
+		return _INSTANCE_DESCENDING;
 	}
 
 	@Override
@@ -75,6 +81,18 @@ public class StructureLinkStructureModifiedDateComparator
 	public boolean isAscending() {
 		return _ascending;
 	}
+
+	private StructureLinkStructureModifiedDateComparator(boolean ascending) {
+		_ascending = ascending;
+	}
+
+	private static final StructureLinkStructureModifiedDateComparator
+		_INSTANCE_ASCENDING = new StructureLinkStructureModifiedDateComparator(
+			true);
+
+	private static final StructureLinkStructureModifiedDateComparator
+		_INSTANCE_DESCENDING = new StructureLinkStructureModifiedDateComparator(
+			false);
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		StructureLinkStructureModifiedDateComparator.class);
