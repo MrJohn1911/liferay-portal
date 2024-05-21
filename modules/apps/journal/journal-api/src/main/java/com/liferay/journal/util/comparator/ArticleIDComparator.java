2325dbd34a9cee9310aebdd9d9642120a1c86a20
Joao Victor <joao.victor@liferay.com>
2024-05-21 17:51:15 -0300

    LPD-26438 Transform ArticleIDComparator into a singleton, make the constructor private and apply to usages
    # breaking
    
    ## What modules/apps/journal/journal-api/src/main/java/com/liferay/journal/util/comparator/ArticleIDComparator.java
    
    the class is now a singleton
    
    ## Why
    
    this change was made to improve the performance of the querys that uses this comparator
    
    ----

--- modules/apps/journal/journal-api/src/main/java/com/liferay/journal/util/comparator/ArticleIDComparator.java
+++ modules/apps/journal/journal-api/src/main/java/com/liferay/journal/util/comparator/ArticleIDComparator.java
@@ -22,8 +22,12 @@ public class ArticleIDComparator extends OrderByComparator<JournalArticle> {
 
 	public static final String[] ORDER_BY_FIELDS = {"articleId", "version"};
 
-	public ArticleIDComparator(boolean ascending) {
-		_ascending = ascending;
+	public static ArticleIDComparator getInstance(boolean ascending) {
+		if (ascending) {
+			return _INSTANCE_ASCENDING;
+		}
+
+		return _INSTANCE_DESCENDING;
 	}
 
 	@Override
@@ -67,6 +71,16 @@ public class ArticleIDComparator extends OrderByComparator<JournalArticle> {
 	public boolean isAscending() {
 		return _ascending;
 	}
+
+	private ArticleIDComparator(boolean ascending) {
+		_ascending = ascending;
+	}
+
+	private static final ArticleIDComparator _INSTANCE_ASCENDING =
+		new ArticleIDComparator(true);
+
+	private static final ArticleIDComparator _INSTANCE_DESCENDING =
+		new ArticleIDComparator(false);
 
 	private final boolean _ascending;
 
