20e90e0aefe8465c56e0b8e03b85895a75ab5d34
Gislayne Vitorino <gislayne.vitorino@liferay.com>
2025-08-04 09:14:52 -0300

    LPD-62032 wip

--- modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/model/listener/CProductModelListener.java
+++ modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/model/listener/CProductModelListener.java
@@ -7,11 +7,20 @@ package com.liferay.commerce.internal.model.listener;
 
 import com.liferay.commerce.product.model.CProduct;
 import com.liferay.commerce.service.CPDAvailabilityEstimateLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.ModelListener;
 
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.kernel.util.UnicodePropertiesBuilder;
+import com.liferay.site.navigation.model.SiteNavigationMenu;
+import com.liferay.site.navigation.model.SiteNavigationMenuItem;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+
+import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Alessio Antonio Rendina
@@ -25,8 +34,54 @@ public class CProductModelListener extends BaseModelListener<CProduct> {
 			deleteCPDAvailabilityEstimateByCProductId(cProduct.getCProductId());
 	}
 
+	@Override
+	public void onAfterUpdate(CProduct originalCProduct, CProduct cProduct) {
+
+		if (Objects.equals(originalCProduct.getExternalReferenceCode(), cProduct.getExternalReferenceCode()) {
+			_updateSiteNavigationMenuItem(originalCProduct.getExternalReferenceCode(), cProduct);
+		}
+	}
+
+
+	private void _updateSiteNavigationMenuItem(String externalReferenceCode, CProduct cProduct)
+		throws PortalException {
+
+		List<SiteNavigationMenu> siteNavigationMenus =
+			_siteNavigationMenuLocalService.getSiteNavigationMenus(
+				cProduct.getGroupId());
+
+		List<SiteNavigationMenuItem> siteNavigationMenuItems =
+			_siteNavigationMenuItemLocalService.getSiteNavigationMenuItems(
+				siteNavigationMenu.getSiteNavigationMenuId());
+
+		for (SiteNavigationMenuItem siteNavigationMenuItem :
+			siteNavigationMenuItems) {
+
+			UnicodeProperties unicodeProperties =
+				UnicodePropertiesBuilder.fastLoad(
+					siteNavigationMenuItem.getTypeSettings()
+				).build();
+
+			String layoutUuid = unicodeProperties.getProperty("layoutUuid");
+
+			if (Objects.equals(layout.getUuid(), layoutUuid)) {
+				_siteNavigationMenuItemLocalService.
+					deleteSiteNavigationMenuItem(
+						siteNavigationMenuItem.getSiteNavigationMenuItemId());
+			}
+		}
+	}
+
 	@Reference
 	private CPDAvailabilityEstimateLocalService
 		_cpdAvailabilityEstimateLocalService;
+
+	@Reference
+	private SiteNavigationMenuItemLocalService
+		_siteNavigationMenuItemLocalService;
+
+	@Reference
+	private SiteNavigationMenuLocalService
+		_siteNavigationMenuLocalService;
 
 }
