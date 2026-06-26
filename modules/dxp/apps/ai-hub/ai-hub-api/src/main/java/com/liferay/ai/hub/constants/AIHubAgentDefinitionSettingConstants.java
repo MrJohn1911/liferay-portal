c776165486cc487b7d46dbe259d3ff93f0ec3637
Mylena Monte <mylena.monte@liferay.com>
2026-06-26 15:58:07 -0300

    LPD-93863 Add the agent definition setting account field constant

---
+++ modules/dxp/apps/ai-hub/ai-hub-api/src/main/java/com/liferay/ai/hub/constants/AIHubAgentDefinitionSettingConstants.java
@@ -0,0 +1,16 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.ai.hub.constants;
+
+/**
+ * @author Mylena Monte
+ */
+public class AIHubAgentDefinitionSettingConstants {
+
+	public static final String OBJECT_FIELD_NAME_ACCOUNT_ENTRY_ID =
+		"r_accountToAIHubAgentDefinitionSettings_accountEntryId";
+
+}
